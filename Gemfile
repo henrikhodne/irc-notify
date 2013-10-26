9cd6b45efc1ed5b924a66fe9b8ce064f221d17dd
Henrik Hodne <henrik@hodne.io>
2013-10-25 20:22:28 -0500

    Add coveralls

--- Gemfile
+++ Gemfile
@@ -2,6 +2,10 @@ source "https://rubygems.org"
 
 gemspec
 
+group :test do
+  gem "coveralls", require: false
+end
+
 platform :rbx do
   gem "rubysl"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 GEM_ROOT = File.expand_path("../../", __FILE__)
 $:.unshift(File.join(GEM_ROOT, "lib"))
 
+if ENV["TRAVIS"]
+  require "coveralls"
+  Coveralls.wear!
+end
+
 require "irc-notify"
